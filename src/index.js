d1dc2f2ffb4cfdca64bc08e1ea621521a934be82
Ubuntu <ubuntu@ip-172-31-93-89.ec2.internal>
2021-07-06 00:58:25 +0000

    fix: test CI automation and work flow

--- src/index.js
+++ src/index.js
@@ -1,14 +1,7 @@
-import '@fortawesome/fontawesome-free/js/fontawesome'
-import '@fortawesome/fontawesome-free/js/solid'
-import '@fortawesome/fontawesome-free/js/regular'
-import '@fortawesome/fontawesome-free/js/brands'
 import 'font-awesome/css/font-awesome.css';
 
 const CoCreateFontAwesome = {
   
-	
 }
-
-// CoCreateFontAwesome.init();
 
 export default CoCreateFontAwesome;
